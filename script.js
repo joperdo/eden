d235526e936c7a5649511f0a71f6f07f2f71ae69
João Pedro Brito <joaopedrobfs@gmail.com>
2024-03-24 20:41:30 -0300

    commit 0.6

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const initSlider = () => {
     const imageList = document.querySelector(".minhasplantas .image-list");
     const slideButtons = document.querySelectorAll(".minhasplantas .slide-button");
 
+
+    //carrossel de imagens de acordo com os botões de click
     slideButtons.forEach(button => {
         button.addEventListener("click", () => {
             const direction = button.id === "prev-slide" ? -1 : 1;
@@ -12,3 +14,18 @@ const initSlider = () => {
 }
 
 window.addEventListener("load", initSlider);
+
+
+//fazer a section-home sumir a medida que página rola
+window.addEventListener('scroll', function() {
+    var sectionHome = document.querySelector('.section-home');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+        sectionHome.classList.add('hidden');
+    } else {
+        sectionHome.classList.remove('hidden');
+    }
+});
+
+
