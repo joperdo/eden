48e39a6b347f596a71e6e2c0ad3a35414dce5ddb
João Pedro Brito <joaopedrobfs@gmail.com>
2024-04-13 18:18:21 -0300

    commit 2.5

--- script.js
+++ script.js
@@ -46,48 +46,95 @@ document.addEventListener("DOMContentLoaded", function() {
   closeModalFoto();
 });
 
+// Chama a função para carregar os casos quando a página for carregada
+document.addEventListener("DOMContentLoaded", function() {
+  loadMinhasPlantas();
+});
 
+// Cria a div "mp" puxando informações do json
+function loadMinhasPlantas() {
+  fetch("minhasplantas.json")
+  .then(response => response.json())
+  .then(data => {
+    const casesContainer = document.querySelector(".image-list");
+    casesContainer.innerHTML = ""; // Limpa a lista
 
-
-// Função para obter acesso à câmera e capturar uma foto
-function capturePhoto() {
-  // Opções para a captura de mídia
-  const mediaOptions = { video: true };
-
-  // Tenta obter o acesso à câmera
-  navigator.mediaDevices.getUserMedia(mediaOptions)
-      .then(function(stream) {
-          // Sucesso ao obter acesso à câmera
-          const track = stream.getVideoTracks()[0]; // Obtém o primeiro track de vídeo do fluxo
-          const imageCapture = new ImageCapture(track); // Cria um objeto ImageCapture com o track de vídeo
-
-          // Captura uma foto
-          imageCapture.takePhoto()
-              .then(function(blob) {
-                  // Sucesso ao capturar a foto
-                  const imageUrl = URL.createObjectURL(blob); // URL da foto capturada
-                  // Exibe a foto em algum lugar da página, por exemplo:
-                  const imgElement = document.createElement('img');
-                  imgElement.src = imageUrl;
-                  document.body.appendChild(imgElement);
-
-                  // Encerra o acesso à câmera após capturar a foto
-                  track.stop();
-              })
-              .catch(function(error) {
-                  // Ocorreu um erro ao capturar a foto
-                  console.error('Error capturing photo:', error);
-              });
-      })
-      .catch(function(error) {
-          // Ocorreu um erro ao tentar obter acesso à câmera
-          console.error('Error accessing the camera:', error);
-      });
+    data.forEach(caseItem => {
+      const div = document.createElement("div");
+      div.className = "mp";
+      div.innerHTML = `<div class="capa">
+                          <div class="capaimg">
+                          <img src="${caseItem.capa}" alt="Capa">
+                          </div>
+                          <div class="coracaov">
+                          <img src="assets/corev.png" alt="Coração vermelho">
+                          </div>
+                      </div>
+                      <div class="titulo">
+                          <div class="iplanta">
+                              <img src="assets/planta.png" alt="Ícone de planta">
+                          </div>
+                          <div class="tplanta">
+                              <h3>${caseItem.title}</h3>
+                          </div>
+                      </div>
+                      <div class="infos">
+                          <div class="icones">
+                              <img src="assets/sol.png" alt="Ícone de sol">
+                          </div>
+                          <div class="infosp">
+                              <h4>${caseItem.luz}</h4>
+                          </div>
+                      </div>
+                      <div class="infos">
+                          <div class="icones">
+                              <img src="assets/gota.png" alt="Ícone de gota">
+                          </div>
+                          <div class="infosp">
+                              <h4>${caseItem.agua}</h4>
+                          </div>
+                      </div>`;
+      casesContainer.appendChild(div);
+    });
+  })
+  .catch(error => {
+    console.error("Erro ao carregar casos:", error);
+  });
 }
 
-// Chamada da função para capturar uma foto
-capturePhoto();
+// Chama a função para carregar os casos quando a página for carregada
+document.addEventListener("DOMContentLoaded", function() {
+  loadNovasPlantas();
+});
 
-  
+// Cria a div "np" puxando informações do json
+function loadNovasPlantas() {
+  fetch("novasplantas.json")
+  .then(response => response.json())
+  .then(data => {
+    const casesContainer = document.querySelector(".novasplantas");
+    casesContainer.innerHTML = ""; // Limpa a lista
+
+    data.forEach(caseItem => {
+      const div = document.createElement("div");
+      div.className = "np";
+      div.innerHTML = `<div class="capan">
+                          <img src="${caseItem.capa}">
+                      </div>
+                      <div class="titulon">
+                          <div class="coracaoc">
+                              <img src="assets/corec.png">
+                              </div>
+                          <div class="tplantan">
+                              <h3>${caseItem.title}</h3>
+                          </div>
+                      </div>`;
+      casesContainer.appendChild(div);
+    });
+  })
+  .catch(error => {
+    console.error("Erro ao carregar casos:", error);
+  });
+}
 
 
